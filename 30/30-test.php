0c0b338157738b8ef7530507c0412b9d279fbc6e
DoanMinhTri <tri.minh.it.0297@gmail.com>
2020-06-26 20:03:33 +0700

    module test 23/6/2020

---
+++ 30/30-test.php
@@ -0,0 +1,131 @@
+<div class="style-30 mt-4">
+    <div class="container">
+        <div class="text-center font-italic text-success my-2">Online Store</div>
+        <div class="text-center my-2">
+            <h2 class="font-weight-bold">Popular Products</h2>
+        </div>
+        <div class="text-center mx-auto w-50 my-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed animi
+            nostrum, quis
+            laudantium moles</div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-12 text-center">
+                <div class="tri">
+                    <div class="box-content">
+                        <div class="img">
+                            <img src="img/img-2.png" alt="">
+                        </div>
+                        <div class="icon text-center my-1">
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-black-50"></i>
+                        </div>
+                        <h3 class="font-weight-bold my-2">Chinese</h3>
+                        <div class="text-overlow">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eos. Iure magnam
+                            expedita,
+                            facilis obcaecati recusandae modi velit earum nobis? Autem, nobis. Exercitationem qui a,
+                            ipsa
+                            aliquam ipsum odit dolore.
+                        </div>
+                        <div class="text-success my-2">
+                            <i class="fas fa-dollar-sign"></i>
+                            <span class="font-weight-bold">500k</span>
+                        </div>
+
+                    </div>
+                    <div class="infor">
+                        <a href="" class="text-decoration-none"><span>More Infor</span>
+                            <i class="fas fa-chevron-circle-right"></i></a>
+                    </div>
+                    <div class="cart">
+                        <a href="" class="text-decoration-none"><span>Add to Cart</span>
+                            <i class="fas fa-shopping-bag"></i></a>
+                    </div>
+                    <div class="gach-chan"></div>
+                </div>
+
+            </div>
+            <div class="col-lg-4 col-sm-12 text-center">
+                <div class="tri">
+                    <div class="box-content">
+                        <div class="img">
+                            <img src="img/img-3.png" alt="">
+                        </div>
+                        <div class="icon text-center my-1">
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-black-50"></i>
+                        </div>
+                        <h3 class="font-weight-bold my-2">Korean</h3>
+                        <div class="text-overlow">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eos. Iure magnam
+                            expedita,
+                            facilis obcaecati recusandae modi velit earum nobis? Autem, nobis. Exercitationem qui a,
+                            ipsa
+                            aliquam ipsum odit dolore.
+                        </div>
+                        <div class="text-success my-2">
+                            <i class="fas fa-dollar-sign"></i>
+                            <span class="font-weight-bold">700k</span>
+                        </div>
+
+                    </div>
+                    <div class="infor">
+                        <a href="" class="text-decoration-none"><span>More Infor</span>
+                            <i class="fas fa-chevron-circle-right"></i></a>
+                    </div>
+                    <div class="cart">
+                        <a href="" class="text-decoration-none"><span>Add to Cart</span>
+                            <i class="fas fa-shopping-bag"></i></a>
+                    </div>
+                    <div class="gach-chan"></div>
+                </div>
+
+            </div>
+            <div class="col-lg-4 col-sm-12 text-center">
+                <div class="tri">
+                    <div class="box-content">
+                        <div class="img">
+                            <img src="img/img-4.png" alt="">
+                        </div>
+                        <div class="icon text-center my-1">
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-warning"></i>
+                            <i class="fas fa-star text-black-50"></i>
+                        </div>
+                        <h3 class="font-weight-bold my-2">Chinese</h3>
+                        <div class="text-overlow">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eos. Iure magnam
+                            expedita,
+                            facilis obcaecati recusandae modi velit earum nobis? Autem, nobis. Exercitationem qui a,
+                            ipsa
+                            aliquam ipsum odit dolore.
+                        </div>
+                        <div class="text-success my-2">
+                            <i class="fas fa-dollar-sign"></i>
+                            <span class="font-weight-bold">850k</span>
+                        </div>
+
+                    </div>
+                    <div class="infor">
+                        <a href="" class="text-decoration-none"><span>More Infor</span>
+                            <i class="fas fa-chevron-circle-right"></i></a>
+                    </div>
+                    <div class="cart">
+                        <a href="" class="text-decoration-none"><span>Add to Cart</span>
+                            <i class="fas fa-shopping-bag"></i></a>
+                    </div>
+                    <div class="gach-chan"></div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div style="height: 100px"></div>
+</div>
---
+++ 30/30.php
@@ -0,0 +1,24 @@
+<?php
+    require_once'lib/scss.inc.php';
+    use ScssPhp\ScssPhp\Compiler;
+    $scss = new Compiler();
+    $str = file_get_contents('sass/30.scss');
+    $str = $scss->compile($str);
+    file_put_contents('css/30.css', $str);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/30.css">
+    <link rel="stylesheet" href="fonts/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>30 sreenshort</title>
+</head>
+
+<body>
+    <?php include '30-test.php' ?>
+</body>
+</html>
