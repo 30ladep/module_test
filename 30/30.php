28e696ea971ced0e5807338e8b1a9180ab608b42
DoanMinhTri <tri.minh.it.0297@gmail.com>
2020-06-27 09:15:01 +0700

    del fonts redundancy

--- 30/30.php
+++ 30/30.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/30.css">
-    <link rel="stylesheet" href="fonts/css/all.min.css">
+    <link rel="stylesheet" href="fonts/all.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>30 sreenshort</title>
 </head>
